01c5da3c23dd6b2504d09dbada2f66e16ae151d4
Finger <Finger@spianmo.com>
2021-04-02 09:12:01 +0800

    完成登录注册的Servlet，完成注册登录相关Service层和Dao层方法,添加lombok依赖,完成Admin角色和普通User跳转不同主页,添加过滤器

---
+++ src/main/java/com/kirito666/JavaWebExample/servlet/AdminLoginServlet.java
@@ -0,0 +1,39 @@
+package com.kirito666.JavaWebExample.servlet;
+
+import com.kirito666.JavaWebExample.service.LoginService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+
+@WebServlet(name = "AdminLoginServlet", urlPatterns = "/admin/login")
+public class AdminLoginServlet extends HttpServlet {
+
+    private LoginService loginService = new LoginService();
+
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req,
+                          HttpServletResponse resp) throws ServletException, IOException {
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        String result = loginService.adminLogin(username, password,
+                req.getSession());
+        if ("1".equals(result)) {
+            resp.sendRedirect("/admin/main.jsp");
+        } else {
+            req.getRequestDispatcher("/index.jsp?message=" + URLEncoder.encode(result, "utf-8")).forward(req, resp);
+        }
+    }
+}
