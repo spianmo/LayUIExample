3f60ad96b6a37f8a7bfb21560f67fa992a0ca9cc
Finger <Finger@spianmo.com>
2021-05-22 23:40:13 +0800

    完善留言板展示前端，fix bug:Servlet层借阅图书和借阅历史错位颠倒，删除冗余的userHistory jsp

--- src/main/java/com/kirito666/JavaWebExample/servlet/BorrowBooksServlet.java
+++ src/main/java/com/kirito666/JavaWebExample/servlet/BorrowBooksServlet.java
@@ -1,9 +1,6 @@
 package com.kirito666.JavaWebExample.servlet;
 
-import com.alibaba.fastjson.JSON;
-import com.kirito666.JavaWebExample.bean.Book;
 import com.kirito666.JavaWebExample.service.BookService;
-import org.apache.commons.io.IOUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,9 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.List;
 
 /**
  * @author Finger
@@ -31,12 +25,9 @@ public class BorrowBooksServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setCharacterEncoding("utf-8");
-        String paramJson = IOUtils.toString(req.getInputStream(), StandardCharsets.UTF_8);
-        HashMap<String, Object> parseObject = JSON.parseObject(paramJson, HashMap.class);
-        String param = (String) parseObject.get("uid");
-        int pageNum = (int) parseObject.get("pageNum");
-        int pageSize = (int) parseObject.get("pageSize");
-        List<Book> books = bookService.queryBorrowBook(param,pageNum, pageSize);
-        req.getSession().setAttribute("borrowbooks", books);
+        String uid = req.getParameter("uid");
+        String bookId = req.getParameter("bookId");
+        String result = bookService.borrowBook(uid, bookId);
+        resp.getWriter().print(result);
     }
 }
--- src/main/java/com/kirito666/JavaWebExample/servlet/BorrowHistoryServlet.java
+++ src/main/java/com/kirito666/JavaWebExample/servlet/BorrowHistoryServlet.java
@@ -1,6 +1,9 @@
 package com.kirito666.JavaWebExample.servlet;
 
+import com.alibaba.fastjson.JSON;
+import com.kirito666.JavaWebExample.bean.Book;
 import com.kirito666.JavaWebExample.service.BookService;
+import org.apache.commons.io.IOUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author Finger
@@ -24,9 +30,12 @@ public class BorrowHistoryServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String uid = req.getParameter("uid");
-        String bookId = req.getParameter("bookId");
-        String result = bookService.borrowBook(uid, bookId);
-        resp.getWriter().print(result);
+        String paramJson = IOUtils.toString(req.getInputStream(), StandardCharsets.UTF_8);
+        HashMap<String, Object> parseObject = JSON.parseObject(paramJson, HashMap.class);
+        String param = (String) parseObject.get("uid");
+        int pageNum = (int) parseObject.get("pageNum");
+        int pageSize = (int) parseObject.get("pageSize");
+        List<Book> books = bookService.queryBorrowBook(param,pageNum, pageSize);
+        req.getSession().setAttribute("borrowbooks", books);
     }
 }
